e0bc81cabb416632bdafcdfa2ef794a0201d05d2
joao034 <84355621+joao034@users.noreply.github.com>
2022-11-05 23:55:36 -0500

    consumir datos en la vista

--- src/main/java/com/cursospring/controllers/IndexController.java
+++ src/main/java/com/cursospring/controllers/IndexController.java
@@ -1,15 +1,18 @@
 package com.cursospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index", "/", "/home"})
-	public String index() {
+	@GetMapping({"/index", "/", "/home", ""})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Curso Spring");
 		return "index";
 	}
 	
