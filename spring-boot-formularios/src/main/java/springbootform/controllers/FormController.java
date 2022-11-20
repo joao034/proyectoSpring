6a127d062b9ced27184ddc77094822139865e153
joao034 <84355621+joao034@users.noreply.github.com>
2022-11-20 01:43:25 -0500

    Validaciones manueales

--- spring-boot-formularios/src/main/java/springbootform/controllers/FormController.java
+++ spring-boot-formularios/src/main/java/springbootform/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import springbootform.models.Usuario;
@@ -18,16 +19,18 @@ public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
+		model.addAttribute("usuario", usuario);
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario , BindingResult result, Model model) {
 
 		if(result.hasErrors()){
 			Map<String, String> errores = new HashMap<>();
 			result.getFieldErrors().forEach( err -> {
-				errores.put(err.getField(), "El campo".concat(err.getField().concat(" ").concat(Objects.requireNonNull(err.getDefaultMessage()))));
+				errores.put(err.getField(), "El campo ".concat(err.getField().concat(" ").concat(Objects.requireNonNull(err.getDefaultMessage()))));
 			});
 			model.addAttribute("error", errores);
 			return "form";
